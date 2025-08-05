4e246eff50ad57f433cf8feddd986bd663b44eef
angelius3000 <160941211+angelius3000@users.noreply.github.com>
2025-08-05 16:45:30 -0600

    Make app buttons square and rename dashboard

--- main.php
+++ main.php
@@ -41,17 +41,17 @@ if (!in_array((int)$_SESSION['TIPOUSUARIO'], [1, 2, 3, 4], true)) {
                             <div class="col">
                                 <div class="page-description text-center">
                                     <h1>Aplicaciones</h1>
-                                    <div class="row justify-content-center">
-                                        <div class="col-md-6 mb-3">
-                                            <a href="Repartos.php" class="card text-decoration-none text-dark bg-transparent border-0">
+                                    <div class="row justify-content-center mt-4">
+                                        <div class="col-auto mb-3">
+                                            <a href="Repartos.php" class="card app-card text-decoration-none text-dark bg-transparent border-0">
                                                 <div class="card-body">
                                                     <i class="material-icons-two-tone" style="font-size:72px;">local_shipping</i>
                                                     <h5 class="mt-1">Reparto</h5>
                                                 </div>
                                             </a>
                                         </div>
-                                        <div class="col-md-6 mb-3">
-                                            <a href="charolas.php" class="card text-decoration-none text-dark bg-transparent border-0">
+                                        <div class="col-auto mb-3">
+                                            <a href="charolas.php" class="card app-card text-decoration-none text-dark bg-transparent border-0">
                                                 <div class="card-body">
                                                     <i class="material-icons-two-tone" style="font-size:72px;">view_day</i>
                                                     <h5 class="mt-1">Charolas</h5>
